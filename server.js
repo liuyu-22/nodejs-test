43951f5724db3c7dd333d9da1bb6197ae9dba091
liuyu-22 <63578680+liuyu-22@users.noreply.github.com>
2022-04-14 21:22:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -17,14 +17,13 @@ var server = http.createServer(function(request, response){
   var query = parsedUrl.query
   var method = request.method
 
-  /******** 从这里开始看，上面不要看 ************/
 
   console.log('有个帅哥发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`傻笑`)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
@@ -38,7 +37,6 @@ var server = http.createServer(function(request, response){
     response.end()
   }
 
-  /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
